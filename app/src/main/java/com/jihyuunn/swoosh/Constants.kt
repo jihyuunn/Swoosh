91f6cfdfdb93396cf33617b687b67b67048ef6dd
jihyuunn <791004_@likelion.org>
2019-07-03 17:00:08 +0900

    toggle buttons and send variables data

---
+++ app/src/main/java/com/jihyuunn/swoosh/Constants.kt
@@ -0,0 +1,3 @@
+package com.jihyuunn.swoosh
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/jihyuunn/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/jihyuunn/swoosh/LeagueActivity.kt
@@ -1,12 +1,51 @@
 package com.jihyuunn.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
+
+    fun mensClicked(view: View) {
+        womensBtn.isChecked = false
+        coedBtn.isChecked = false
+
+        selectedLeague = "mens"
+    }
+
+    fun womensClicked(view: View) {
+        mensBtn.isChecked = false
+        coedBtn.isChecked = false
+
+        selectedLeague = "womens"
+    }
+
+    fun coedClicked(view: View) {
+        mensBtn.isChecked = false
+        womensBtn.isChecked = false
+
+        selectedLeague = "co-ed"
+    }
+
+    fun nextClicked(view: View) {
+        if (selectedLeague != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillActivity)
+        } else {
+            Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
